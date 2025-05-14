39162aec6ba3798a7072001ff426789700c53033
michal-entsys <michal@enterprisesystems.co.uk>
2025-05-14 22:47:47 +0100

    Add recipes list + scrolling

--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -10,6 +10,9 @@ import { MatDialog } from '@angular/material/dialog';
 import { NewItemDialogComponent } from 'src/app/shared/new-item-dialog/new-item-dialog.component';
 import { NewRecipeDialogComponent } from 'src/app/shared/new-item-dialogs/new-recipe-dialog/new-recipe-dialog.component';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { InfiniteScrollDirective } from 'ngx-infinite-scroll';
+import { RouterModule } from '@angular/router';
+import { MatSidenav, MatSidenavModule } from '@angular/material/sidenav';
 
 @Component({
     selector: 'app-recipes-list',
@@ -20,47 +23,83 @@ import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
         MatIconModule,
         NewItemDialogComponent,
         NewRecipeDialogComponent,
+        InfiniteScrollDirective,
+        RouterModule,
+        MatSidenavModule,
     ],
     templateUrl: './recipes-list.component.html',
     styleUrl: './recipes-list.component.scss',
 })
 export class RecipesListComponent {
-    readonly dialog = inject(MatDialog);
-    readonly recipeService = inject(RecipesService);
-    // readonly categoryService = inject(RecipesCategoryService);
-    readonly destroyRef = inject(DestroyRef);
+    // readonly dialog = inject(MatDialog);
+    // readonly recipeService = inject(RecipesService);
+    // // readonly categoryService = inject(RecipesCategoryService);
+    // readonly destroyRef = inject(DestroyRef);
 
-    recipeId = input<string>();
-    categoryId = input<string>();
+    // recipeId = input<string>();
+    // categoryId = input<string>();
 
-    recipes$: Observable<RecipeAPI[]> | undefined;
-    // category$: Observable<RecipeCategoryAPI> | undefined;
+    // recipes$: Observable<RecipeAPI[]> | undefined;
+    // // category$: Observable<RecipeCategoryAPI> | undefined;
 
-    constructor() {
-        effect(() => {
-            if (this.categoryId()) {
-                // this.category$ = this.categoryService.getCategory(this.categoryId()!).pipe(takeUntilDestroyed(this.destroyRef));
-                // this.recipes$ = this.recipeService.getRecipesByCategoryPublicId(this.categoryId()!).pipe(
-                // 	takeUntilDestroyed(this.destroyRef),
-                // 	tap(recipes => console.log(recipes.length)),
-                // 	catchError(error => { debugger; throw 'error' + error })
-                // );
-            }
-        });
+    // constructor() {
+    //     effect(() => {
+    //         if (this.categoryId()) {
+    //             // this.category$ = this.categoryService.getCategory(this.categoryId()!).pipe(takeUntilDestroyed(this.destroyRef));
+    //             // this.recipes$ = this.recipeService.getRecipesByCategoryPublicId(this.categoryId()!).pipe(
+    //             // 	takeUntilDestroyed(this.destroyRef),
+    //             // 	tap(recipes => console.log(recipes.length)),
+    //             // 	catchError(error => { debugger; throw 'error' + error })
+    //             // );
+    //         }
+    //     });
+    // }
+
+    // addNewRecipe(): void {
+    //     this.dialog
+    //         .open(NewItemDialogComponent, {
+    //             data: {
+    //                 component: NewRecipeDialogComponent,
+    //                 itemType: 'recipe',
+    //                 // item: this.category$,
+    //             },
+    //         })
+    //         .afterClosed()
+    //         .subscribe((result) => {
+    //             // debugger;
+    //         });
+    // }
+
+    recipes: any[] = [];
+    page = 1;
+    limit = 20;
+    loading = false;
+    noMoreData = false;
+
+    constructor(private recipeService: RecipesService) {}
+
+    ngOnInit(): void {
+        this.loadMore();
     }
 
-    addNewRecipe(): void {
-        this.dialog
-            .open(NewItemDialogComponent, {
-                data: {
-                    component: NewRecipeDialogComponent,
-                    itemType: 'recipe',
-                    // item: this.category$,
-                },
-            })
-            .afterClosed()
-            .subscribe((result) => {
-                // debugger;
+    loadMore(): void {
+        if (this.loading || this.noMoreData) return;
+
+        this.loading = true;
+        this.recipeService
+            .getRecipes(this.page, this.limit)
+            .subscribe((data) => {
+                if (data.length < this.limit) {
+                    this.noMoreData = true;
+                }
+
+                this.recipes.push(...data);
+                this.page++;
+                this.loading = false;
             });
+    }
+
+    onDrawerClosed(): void {
+        console.log('recipe drawer closed');
     }
 }
--- src/app/recipes/recipes-service/recipes.service.ts
+++ src/app/recipes/recipes-service/recipes.service.ts
@@ -2,50 +2,61 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
 import { Observable } from 'rxjs/internal/Observable';
 import { environment } from '../../../environments/environment.development';
+import { delay, of } from 'rxjs';
 
 @Injectable({
-	providedIn: 'root'
+    providedIn: 'root',
 })
 export class RecipesService {
+    http = inject(HttpClient);
+    apiBase = environment.baseUrl;
 
-	http = inject(HttpClient);
-	apiBase = environment.baseUrl;
+    // getRecipeById(recipeId: number): Recipe | undefined {
+    // 	return this._recipes.find(recipe => recipe.id === recipeId);
+    // }
 
-	// getRecipeById(recipeId: number): Recipe | undefined {
-	// 	return this._recipes.find(recipe => recipe.id === recipeId);
-	// }
+    // getRecipesByCategoryPublicId(categoryPid: string): Observable<RecipeAPI[]> {
+    // 	return this.http.get<RecipeAPI[]>(`${environment.baseUrl}/recipe-categories/${categoryPid}/recipes`);
+    // }
 
-	getRecipesByCategoryPublicId(categoryPid: string): Observable<RecipeAPI[]> {
-		return this.http.get<RecipeAPI[]>(`${environment.baseUrl}/recipe-categories/${categoryPid}/recipes`);
-	}
+    getRecipes(page: number, limit: number) {
+        const start = (page - 1) * limit;
+        const end = start + limit;
+        const data = this.allRecipes.slice(start, end);
+        return of(data).pipe(delay(500)); // opóźnienie dla symulacji ładowania
+    }
 
+    private allRecipes = Array.from({ length: 30 }, (_, i) => ({
+        id: i + 1,
+        title: `Przepis #${i + 1}`,
+        description: `To jest krótki opis przepisu numer ${i + 1}.`,
+        kcal: Math.floor(50 + Math.random() * 400),
+        image: `https://placehold.co/300x200?text=Przepis+${i + 1}`,
+    }));
 
+    // getRecipes(): Observable<Recipe[]> {
+    // 	return this.http.get<Recipe[]>(`${this.apiBase}/recipes`);
+    // }
 
+    // getRecipe(recipeId: number): Observable<Recipe> {
+    // 	return this.http.get<Recipe>(`${this.apiBase}/recipes/${recipeId}`);
+    // }
 
-	// getRecipes(): Observable<Recipe[]> {
-	// 	return this.http.get<Recipe[]>(`${this.apiBase}/recipes`);
-	// }
+    // createRecipe(recipeAPI: RecipeAPI): Observable<Recipe> {
+    // 	return this.http.post<Recipe>(`${this.apiBase}/recipes`, recipeAPI);
+    // }
 
-	// getRecipe(recipeId: number): Observable<Recipe> {
-	// 	return this.http.get<Recipe>(`${this.apiBase}/recipes/${recipeId}`);
-	// }
+    // updateRecipe(recipeId: number, recipeAPI: RecipeAPI): Observable<Recipe> {
+    // 	return this.http.put<Recipe>(`${this.apiBase}/recipes/${recipeId}`, recipeAPI);
+    // }
 
-	// createRecipe(recipeAPI: RecipeAPI): Observable<Recipe> {
-	// 	return this.http.post<Recipe>(`${this.apiBase}/recipes`, recipeAPI);
-	// }
-
-	// updateRecipe(recipeId: number, recipeAPI: RecipeAPI): Observable<Recipe> {
-	// 	return this.http.put<Recipe>(`${this.apiBase}/recipes/${recipeId}`, recipeAPI);
-	// }
-
-	// deleteRecipe(recipeId: number): Observable<Recipe> {
-	// 	return this.http.delete<Recipe>(`${this.apiBase}/recipes/${recipeId}`);
-	// }
-
+    // deleteRecipe(recipeId: number): Observable<Recipe> {
+    // 	return this.http.delete<Recipe>(`${this.apiBase}/recipes/${recipeId}`);
+    // }
 }
 
 export interface RecipeAPI {
-	id: number;
-	name: string;
-	description: string;
+    id: number;
+    name: string;
+    description: string;
 }
